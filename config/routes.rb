5631bb58c6030ee1d8a8c63dbcbe817b0ca1982d
alexpfrom <afrom@chicagobooth.edu>
2021-05-20 17:16:47 +0000

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "car_infos#index"
+
   resources :car_infos
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
